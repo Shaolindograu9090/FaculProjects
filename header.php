3437bf6e31113019c555550baf7f169f77f8a1ae
Breno-hardcode <kl7727420@gmail.com>
2025-09-27 15:44:00 -0400

    Create header.php

---
+++ header.php
@@ -0,0 +1,18 @@
+<?php require_once __DIR__ . '/helpers.php'; ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+<meta charset="utf-8">
+<title></title>
+</head>
+<body>
+  <header>
+    <h1></h1>
+    <nav>
+      <a href="index.php">Home</a> |
+      <a href="quem_somos.php">Quem Somos</a> |
+      <a href="produtos.php">Produtos</a> |
+      <a href="contato.php">Contato</a>
+    </nav>
+    <hr>
+  </header>
